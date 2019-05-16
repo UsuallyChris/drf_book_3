45332f40c635eb5a29ab193a26ea17a659a28e94
UsuallyChris <schoonc@gmail.com>
2019-05-16 00:45:03 -0700

    Added permission classes on views

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,5 +1,5 @@
 """ view class definitions for posts app """
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -9,9 +9,11 @@ class PostList(generics.ListCreateAPIView):
     """ Post collection API endpoint """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """ Singe Post endpoint """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,)
